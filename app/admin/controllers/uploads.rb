f5d5a8b00de7f888d094469e94baf9e063d04645
Rajnish <dashora.rajnish@gmail.com>
2015-02-06 17:03:41 +0530

    upload file form creation

--- app/admin/controllers/uploads.rb
+++ app/admin/controllers/uploads.rb
@@ -1,8 +1,19 @@
 ActiveAdmin.register_page 'Uploads' do
   menu priority: 1
+
   controller do
     layout 'active_admin'
+    skip_before_filter :verify_authenticity_token
+
+    def upload
+      #understading the flow for flash and redirection to the index page 
+      flash[:notice] = "Data Imported successfully"
+      redirect_to action: :index
+    end
+
     def index
+
     end
   end
+
 end
