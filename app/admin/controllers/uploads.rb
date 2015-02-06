632b2657576574d0b82c267e1dd9a2e3b4cd372f
Rajnish <dashora.rajnish@gmail.com>
2015-02-06 19:52:24 +0530

    removed skip authentication

--- app/admin/controllers/uploads.rb
+++ app/admin/controllers/uploads.rb
@@ -3,19 +3,19 @@ ActiveAdmin.register_page 'Uploads' do
 
   controller do
     layout 'active_admin'
-    skip_before_filter :verify_authenticity_token
-
     def upload
       file_path=saved_file_path(params[:upload][:file])
       if(file_path)
+        p file_path
         flash[:notice] = "Data Imported successfully"
         redirect_to action: :index
       end
     end
-    
+
     def index
     end
 
+    private
     def saved_file_path(upload_file)
       if upload_file
         FileUtils.rm_f(Rails.root.join('public', 'uploads', '*'))
