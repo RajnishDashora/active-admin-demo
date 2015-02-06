205a2e29bef0b9ba08ee3c7f59f00fa8688f09e9
Rajnish <dashora.rajnish@gmail.com>
2015-02-06 16:36:36 +0530

    uploads tab added

---
+++ app/admin/controllers/uploads.rb
@@ -0,0 +1,8 @@
+ActiveAdmin.register_page 'Uploads' do
+  menu priority: 1
+  controller do
+    layout 'active_admin'
+    def index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  namespace 'admin' do
+    get '/' => 'uploads#index'
+  end
+
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
